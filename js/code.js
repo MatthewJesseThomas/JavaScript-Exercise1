cc5cec5711cfeeefbd340978fec4cab3fa40f4e7
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-18 12:08:52 +0200

    main

--- js/code.js
+++ js/code.js
@@ -1,3 +1,20 @@
-function madeBy() {
-    alert("Made By MatthewJThomas");
-}
+// function madeBy() {
+//     alert("Made By MatthewJThomas");
+// }
+// let message = 'Deviant';
+// document.write(message + '<br>');
+
+// let age = 43;
+// document.write(age + '<br>');
+
+// let isOld = true;
+// document.write(isOld);
+
+let message = 'Deviant';
+let age = 43;
+let isOld = true;
+
+
+// let output = `Message: ` +  message + `<br>Age: ` + age + `<br>Old: `+ isOld;
+let output = `Message: ${message}<br>Age: ${age} <br>Old: ${isOld}`
+document.write(output);
